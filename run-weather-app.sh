664866c6c745f7a16fd4656821693a2d9158698a
Rohit Kumar <rohit@cambridgeSemantics.com>
2017-11-13 14:46:27 -0500

    fixed absolute file path

--- run-weather-app.sh
+++ run-weather-app.sh
@@ -1,6 +1,7 @@
 sudo apt-get install git-all
 git clone "https://github.com/rohitkum2506/klaviyo-weather-email-app.git"
 mv klaviyo-weather-email-app klaviyo
+cd klaviyo
 docker build -t rohit/dockertest .
 docker run -it -p 8000:8000 rohit/dockertest
 
